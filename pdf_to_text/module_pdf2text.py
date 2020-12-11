59b50ae6fdc282255bb6138abbb144c99b288a35
Ilyas <ilyasy1851@gmail.com>
2020-12-11 10:32:42 +0100

    Change URL | added indent

--- pdf_to_text/module_pdf2text.py
+++ pdf_to_text/module_pdf2text.py
@@ -1,7 +1,7 @@
 import pdftotext
 import requests
 
-url = "http://phisapi.nhhost.online:4001/setRecipes"
+url = "http://phisapi.nhhost.online:3000/setRecipes"
 
 
 def main():
@@ -42,9 +42,8 @@ def main():
         'Persons': int(aantal_personen),
         'Ingredients': ingredienten
     }
-    headers = {}
 
-response = requests.request("POST", url, headers=headers, data = json)
+    response = requests.post(url, json=json)
 
     print(response.text.encode('utf8'))
     for x in output.items():
