493ab911444b105eca9aedc65b2d0b9ba33732ba
srphihung <sr.phihung@gmail.com>
2021-01-22 09:11:31 +0100

    Bug fixes & styling recipe page

--- frontend/js/custom.js
+++ frontend/js/custom.js
@@ -1,47 +1,56 @@
-// // const fetch = require("node-fetch");
+function getRecipes(){
+    fetch('http://172.16.1.40:3000/recipes')
+        .then(response => response.json())
+        .then(data => renderRecipes(data));
+}
+function renderRecipes(data){
+    console.log(data)
+    let content  = document.querySelector('#recipes .carousel-inner');
+    for (var i = 0; i < data.length; i++){
+      
+      if (i == 0){
+        content.innerHTML += '<div class="carousel-item active"> <a data-id="' + data[i].CommunityID + '" href="#" class="card recipes"> <div class="card-content"> <h3 id="title">' + data[i].Title + '</h3> <p id="description">' + data[i].Preparation + '</p><div class="card-links"><div class="card-link"><i class="far fa-heart"></i><p id="likes">12</p></div><div class="card-link"><i class="far fa-comment"></i><p id="comments">12</p></div><div class="card-link"><i class="far fa-star"></i><p id="populairity">12</p></div></div></div></a></div>';
+      }
+      else {
+        content.innerHTML += '<div class="carousel-item"> <a data-id="' + data[i].CommunityID + '"href="#" class="card recipes"> <div class="card-content"> <h3 id="title">' + data[i].Title + '</h3> <p id="description">' + data[i].Preparation + '</p><div class="card-links"><div class="card-link"><i class="far fa-heart"></i><p id="likes">12</p></div><div class="card-link"><i class="far fa-comment"></i><p id="comments">12</p></div><div class="card-link"><i class="far fa-star"></i><p id="populairity">12</p></div></div></div></a></div>';
+      }
+    }
+    
+    jQuery(document).ready(function(){
+      jQuery('a.recipes').on('click', function(){               
+        let recipeID = $(this).data("id");
+        getRecipe(recipeID);
+        // console.log(jsonObject);
+                // console.log(jsonObject.CommunityID);
+      });
+    });
+  
+    function getRecipe(id) {
+    fetch('http://172.16.1.40:3000/recipes')
+        .then(response => response.json())
+        .then(data => TESTrenderRecipe(data, id));
+    }
 
-// var CommunityID;
-
-// function getRecipes(count) {
-//     fetch('http://phisapi.nhhost.online:4001/recipes')
-//     .then(response => response.json())
-//     .then(data =>{
-//         // console.log(data)
-//         // var CommunityID = data[count].CommunityID;
-//         // console.log(CommunityID);
-//         // var PDFPath = data[count].PDFPath;
-//         // console.log(PDFPath);
-//         // var Populairity = data[count].Populairity;
-//         // document.getElementById("populairity").innerHTML = Populairity;
-//         // console.log(Populairity);
-//         // var Preparation = data[count].Preparation;
-//         // document.getElementById("preparation").innerHTML = Preparation;
-//         // console.log(Preparation);
-//         // var BannerImageID = data[count].CommunityID;
-//         // console.log(CommunityID);
-//         // var Readtime = data[count].Readtime;
-//         // console.log(Readtime);
-//         // var AverageRanking = data[count].AverageRanking;
-//         // console.log(AverageRanking);
-//         var Likes = data[count].Likes;
-//         document.getElementById("likes").innerHTML = Likes;
-//         console.log(Likes);
-//         var Title = data[count].Title;
-//         document.getElementById("title").innerHTML = Title;
-//         console.log(Title);
-//         // var Step = data[count].Step;
-//         // console.log(Step);
-//         var Persons = data[count].Persons;
-//         console.log(Persons);
-//         var Ingredients = data[count].Ingredients;
-//         console.log(Ingredients);
-//         var Description = data[count].Description;
-//         document.getElementById("description").innerHTML = Description;
-//         console.log(Description);
-//     } )
-// }
-
-// for (count = 0; count < 1; count++) {
-//     getRecipes(count);
-//     // console.log(CommunityID);
-// }
+    function TESTrenderRecipe(data, id) {
+    
+    $.each(data, function (index, jsonObject) {
+      let recipeContent = $('#recipe');
+      let communityContent = $('#community');
+        if (jsonObject.CommunityID == id) {
+            recipeContent.html('<section> <div class="container"> <div class="row"> <div class="col-12"> <div class="recipeHead"> <div class="recipeWrap"> <div class="recipeImg"> <img src="img/recipes.jpg"> </div><img src="img/frame.png"> <div class="faseIcon d-flex align-items-center justify-content-center"> <p class="faseText">Stap</p><p class="faseNr">' + jsonObject.Step + '</p></div></div><h2 id="title">' + jsonObject.Title + '</h2> <div class="views d-flex align-items-center"> <i class="fas fa-eye"></i> <p class="view-count">' + jsonObject.AverageRanking + '</p></div></div><div class="recipeContent"> <h4>Dit recept bevat</h4> <p>2/3 portie groente | 1 portie eiwit |1/5 portie zuivel</p><h4>Ingrediënten</h4> <p>' + jsonObject.Ingredients + '</p><h4>Bereiding</h4> <p id="preparation">' + jsonObject.Preparation + '</p></div><div class="recipeLink text-center"> <a href="#" class="link"> <i class="far fa-heart"></i> </a> <a href="#" class="link"> <i class="far fa-comment"></i> </a> <a href="#" class="link"> <i class="far fa-star"></i> </a> <i class="fas fa-times closeRecipe" style="display: block;padding: 15px;font-size: 30px;color: #dcdcdc;"></i> </div></div></div></div></section>');
+            
+            recipeContent.toggle();
+            communityContent.toggle();
+        }
+      });
+      
+      jQuery(document).ready(function(){
+        let communityContent = $('#community');
+        jQuery('.closeRecipe').on('click', function(){               
+          $('#recipe').hide();
+          communityContent.toggle();
+        });
+      });
+    }
+}
+getRecipes()
